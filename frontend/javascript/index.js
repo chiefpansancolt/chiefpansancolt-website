fedb70ac9c80d4d8eee3a405ec36b1d436aa5033
Christopher Pezza <chris.pezza@hey.com>
2020-08-05 21:18:45 -0400

    Update Tabular function

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -25,7 +25,8 @@ var afterClick = function () {
     const previousTab = results.dataset.tabularPreviousTab
 
     if(selectedTab != previousTab) {
-        
+        document.getElementById(selectedTab).classList = "w-1/4 py-4 px-1 text-center border-b-2 border-indigo-500 font-medium text-sm leading-5 text-indigo-600 focus:outline-none focus:text-indigo-800 focus:border-indigo-700"
+        document.getElementById(previousTab).classList = "w-1/4 py-4 px-1 text-center border-b-2 border-transparent font-medium text-sm leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300"
     }
 };
 
