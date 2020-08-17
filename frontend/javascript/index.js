30bd21595c95797766f6a77a1d4efcbc4574a529
Christopher Pezza <chris.pezza@hey.com>
2020-08-17 00:05:21 -0400

    Add Pop Collection

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -19,15 +19,79 @@ document.addEventListener("DOMContentLoaded", () => {
 
 var tabNav = document.getElementById("tabNavigation")
 
-var afterClick = function () {
-    const results = document.querySelector("#tabNavigation")
-    const selectedTab = results.dataset.tabularSelectedTab
-    const previousTab = results.dataset.tabularPreviousTab
+if(tabNav) {
+    var afterClick = function () {
+        const results = document.querySelector("#tabNavigation")
+        const selectedTab = results.dataset.tabularSelectedTab
+        const previousTab = results.dataset.tabularPreviousTab
 
-    if(selectedTab != previousTab) {
-        document.getElementById(selectedTab).classList = "w-1/4 py-4 px-1 text-center border-b-2 border-indigo-500 font-medium text-sm leading-5 text-indigo-600 focus:outline-none focus:text-indigo-800 focus:border-indigo-700"
-        document.getElementById(previousTab).classList = "w-1/4 py-4 px-1 text-center border-b-2 border-transparent font-medium text-sm leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300"
+        if(selectedTab != previousTab) {
+            document.getElementById(selectedTab).classList = "w-1/4 py-4 px-1 text-center border-b-2 border-indigo-500 font-medium text-sm leading-5 text-indigo-600 focus:outline-none focus:text-indigo-800 focus:border-indigo-700"
+            document.getElementById(previousTab).classList = "w-1/4 py-4 px-1 text-center border-b-2 border-transparent font-medium text-sm leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300"
+        }
+    };
+
+    tabNav.addEventListener('click', afterClick, false);
+}
+
+var mybutton = document.getElementById("back-to-top");
+
+if(mybutton) {
+    window.onscroll = function () {
+        scrollFunction()
+    };
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
     }
-};
 
-tabNav.addEventListener('click', afterClick, false);
+    const scrollToTop = () => {
+        const c = document.documentElement.scrollTop || document.body.scrollTop;
+        if (c > 0) {
+            window.requestAnimationFrame(scrollToTop);
+            window.scrollTo(0, c - c / 25);
+        }
+    };
+    mybutton.addEventListener('click', scrollToTop, false);
+}
+
+var tags = document.querySelectorAll('.pop-badge');
+var popList = document.querySelectorAll('.pop');
+var dotSvg = '<svg class="-ml-0.5 mr-1.5 h-2 w-2 text-orange-400" fill="currentColor" viewBox="0 0 8 8"><circle cx="4" cy="4" r="3" /></svg>';
+
+var tagClick = function(event) {
+    const name = event.target.dataset.name;
+    for (let index = 0; index < tags.length; index++) {
+        var element = tags[index];
+        if (element.childNodes.length > 1) {
+            element.childNodes[0].remove();
+        }
+    }
+    
+    event.target.insertAdjacentHTML('afterbegin', dotSvg);
+    for (let index = 0; index < popList.length; index++) {
+        var element = popList[index];
+        if(name == "All") {
+            console.log('In All');
+            element.style.display = "block";
+        } else {
+            console.log(element.dataset.group);
+            if(element.dataset.group != name) {
+                console.log('To hide');
+                element.style.display = "none";
+            } else {
+                console.log('Not To hide');
+                element.style.display = "block";
+            }
+        }
+    }
+}
+
+for (let index = 0; index < tags.length; index++) {
+    const element = tags[index];
+    element.addEventListener('click', tagClick, false);
+}
---
+++ src/_pops/patronus-hermione.md
@@ -0,0 +1,7 @@
+---
+name: Patronus Hermione
+img: https://cdn.shopify.com/s/files/1/1052/2158/products/46996_HarryPotter_Patronus-Hermione-Granger_PR_WEB_600x2000.png?v=1592286
+item-number: 46996
+number: 106
+group: Harry Potter
+---
