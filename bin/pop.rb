a886a49a85eab45d6d51fd6e4a58b333ab88123d
Christopher Pezza <chris.pezza@hey.com>
2020-08-29 21:54:10 -0400

    Touch up script

--- bin/pop.rb
+++ bin/pop.rb
@@ -12,16 +12,18 @@ class Pop < Thor::Group
         say("What is the Name of the Pop!")
         name_entry = ask("Pop Name: ")
 
-        say("\nWhat is the url to the Pop! Image")
-        img_url = ask("URL: ", path: true)
+        say("\nWhat is the img ext?")
+        ext_entry = ask("extension: ", default: "jpg")
 
         say("\nWhat number is the Pop!")
         number_entry = ask("Number: ")
 
         say("\nWhat Group is this apart of")
-        group_entry = ask("Group", limited_to: ["Marvel", "Game of Thrones", "Harry Potter", "Star Wars", "Disney", "Fortnite", "Pokemon", "Jurrassic Park"])
+        group_entry = ask("Group", limited_to: ["Marvel", "Game of Thrones", "Harry Potter", "Star Wars", "Disney", "Fortnite", "Pokemon", "Jurrassic Park", "Borderlands"])
 
         say("\nGenerated:!")
+
+        img_url = "/assets/images/pops/" + name_entry.gsub(/\s+/, "-").downcase + "-" + number_entry + "." + ext_entry
 
         opts = { name: name_entry, img: img_url, number: number_entry, group: group_entry }
         template "templates/pop_template.tt", "./src/_pops/#{name_entry.gsub(/\s+/, "-").downcase + "-" + number_entry}.md", opts
